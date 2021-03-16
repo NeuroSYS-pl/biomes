74129c147574fbd88de71bc7845456ab19c3e3ac
Jakub Błaszczyk <20283462+jamebs@users.noreply.github.com>
2021-03-16 13:53:27 +0100

    Setup files structure

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: [
+      './tsconfig.eslint.json',
+      './packages/*/tsconfig.json',
+    ],
+  },
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'react'
+  ],
+  rules: {
+    'class-methods-use-this': 'off',
+    'no-else-return': [
+      'error',
+      { allowElseIf: true },
+    ],
+    'import/no-cycle': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_\\d*$' },
+    ],
+    '@typescript-eslint/lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.test.ts',
+          '**/*.spec.ts',
+          '**/*.e2e-spec.ts',
+        ],
+      },
+    ],
+    'prettier/prettier': 'error',
+  },
+};
